aa2fd72f0fa3f93596c224f8a532c82d5c0e7812
Asliddin Azizovich <space.coding.programmer@gmail.com>
2021-06-19 03:38:27 +0500

    yii2 advanced api template

---
+++ common/config/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+Yii::setAlias('@common', dirname(__DIR__));
+Yii::setAlias('@api', dirname(dirname(__DIR__)) . '/api');
+Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
+Yii::setAlias('@uploads', dirname(dirname(__DIR__)) . '/uploads');
